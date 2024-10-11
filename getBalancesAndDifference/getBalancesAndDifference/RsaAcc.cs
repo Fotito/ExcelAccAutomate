1962c8cd3d06c1f12baee316ffc33cc2cc232b09
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-10-11 16:03:58 +0200

    Update RsaAcc.cs

--- getBalancesAndDifference/getBalancesAndDifference/RsaAcc.cs
+++ getBalancesAndDifference/getBalancesAndDifference/RsaAcc.cs
@@ -14,19 +14,17 @@ namespace getBalancesAndDifference
        private string     Accountnumber;
        private string DebtNumber;
         private string IDNumber;
-        private string Names;
         private string Surname;
         private string Initial;
         private string Title;
          private double   CapitalAmt;
           private double  BalOutstanding;
-//1,4.5.6.7.8.9.10.11
-        public RsaAcc(string Accnum, string debtNum, string IDNum, string name, string surname, string initial, string title,
+//1,4.5.6.8.9.10.11
+        public RsaAcc(string Accnum, string debtNum, string IDNum, string surname, string initial, string title,
             double capAmmoun, double balanceOutstanding) {
         this.Accountnumber = Accnum;
             this.DebtNumber = debtNum;
             this.IDNumber = IDNum;
-            this.Names = name;
             this.Surname = surname;
             this.Initial = initial;
             this.Title = title;
